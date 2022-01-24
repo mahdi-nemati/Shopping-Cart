3eebb84736758f3d9741cf628d638389c7941c6e
mahdi <this.nemati@gmail.com>
2022-01-24 13:59:40 +0330

    add cart and their dispatch

--- src/Pages/CardPage.js
+++ src/Pages/CardPage.js
@@ -1,9 +1,64 @@
+import { Link } from "react-router-dom";
 import Layout from "../Layout/Layout";
-
+import { useCart } from "../Providers/CartProvider";
+import { useCartActions } from "../Providers/CartProvider";
+import "./CartItem.css";
 const CardPage = () => {
-    return ( <Layout>
-        <h2>cart page</h2>
-    </Layout> );
-}
- 
+  const { cart } = useCart();
+  const dispatch = useCartActions();
+  // increment
+  const incHandler = (item) => {
+    dispatch({
+      type: "increment",
+      payload: item,
+    });
+  };
+  // decrement
+  const decHandler = (item) => {
+    dispatch({
+      type: "decrement",
+      payload: item,
+    });
+  };
+
+  if (!cart.length)
+    return (
+      <Layout>
+        <main>
+          <h3>no product in cart</h3>
+          <Link to="/">Home</Link>
+        </main>
+      </Layout>
+    );
+  return (
+    <Layout>
+      <main className="container">
+        <section className="cartCenter">
+          <section className="cartItemList">
+            {cart.map((item) => {
+              return (
+                <div key={item.id} className="cartItem">
+                  <div className="cartImage">
+                    <img src={item.image} alt={item.name} />
+                  </div>
+                  <div className="cartDetails">
+                    <div>{item.name}</div>
+                    <div> $ {item.price * item.quantity}</div>
+                    <div className="cartBtn">
+                      <button onClick={() => decHandler(item)}>remove</button>
+                      <p> {item.quantity}</p>
+                      <button onClick={() => incHandler(item)}>Add</button>
+                    </div>
+                  </div>
+                </div>
+              );
+            })}
+          </section>
+          <section className="summery">summery</section>
+        </section>
+      </main>
+    </Layout>
+  );
+};
+
 export default CardPage;
--- src/Providers/CartReducer.jsx
+++ src/Providers/CartReducer.jsx
@@ -13,6 +13,32 @@ const CartReducer = (state, action) => {
         updatedCart[index] = updatedItem;
       }
       return { ...state, cart: updatedCart };
+    // increment quantity
+    case "increment":
+      const updatedInc = [...state.cart];
+      const indexOfItem = updatedInc.findIndex(
+        (item) => item.id === action.payload.id
+      );
+      const selectedInc = { ...updatedInc[indexOfItem] };
+      selectedInc.quantity++;
+      updatedInc[indexOfItem] = selectedInc;
+      return { ...state, cart: updatedInc };
+    // decrement
+    case "decrement":
+      const updatedDec = [...state.cart];
+      const indexOfItemDec = updatedDec.findIndex(
+        (item) => item.id === action.payload.id
+      );
+      const selectedDec = { ...updatedDec[indexOfItemDec] };
+      if (selectedDec.quantity === 1) {
+        const filterDec = updatedDec.filter(
+          (item) => item.id !== action.payload.id
+        );
+        return { ...state, cart: filterDec };
+      }
+      selectedDec.quantity--;
+      updatedDec[indexOfItemDec] = selectedDec;
+      return { ...state, cart: updatedDec };
     default:
       return state;
   }
