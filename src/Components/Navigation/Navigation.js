990ea3429d45a15dded9537ecb8547294357f243
mahdi <this.nemati@gmail.com>
2022-01-28 19:35:32 +0330

    home page redesign and responsiving

--- src/Components/Navigation/Navigation.js
+++ src/Components/Navigation/Navigation.js
@@ -35,7 +35,10 @@ const Navigation = () => {
                 className={(n) => (n.isActive ? `${styles.active}` : "")}
               >
                 <FaShoppingCart />
-                {cart.length > 0 ? <span> {cart.length}</span> : ""}
+                {cart.length > 0 ? <span class="absolute flex justify-center 
+                items-center text-rose-50 bg-red-400 
+                rounded-full w-6 h-6 text-lg top-1 right-8 sm:right-20 sm:w-7 sm:h-7 sm:text-xl
+                md:right-24 md:top-2 lg:right-36 lg:text-2xl lg:w-9 lg:h-9"> {cart.length}</span> : ""}
               </NavLink>
             </li>
             <li class="ml-4 sm:ml-8 md:ml-10 lg:ml-16">
--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -18,19 +18,34 @@ const HomePage = () => {
   };
   return (
     <Layout>
-      <main>
-        <section class="flex flex-wrap">
+      <main class="mt-7 md:mt-12 lg:mt-20 bg-slate-100">
+        <section class="grid grid-cols-1 justify-items-center sm:grid-cols-2 lg:grid-cols-3">
           {data.products.map((product) => {
             return (
-              <section key={product.id}>
-                <div class="">
-                  <img src={product.image} alt={product.name}></img>
+              <section
+                key={product.id}
+                class="flex flex-col mt-14
+              w-10/12 bg-teal-100 rounded-xl pb-4"
+              >
+                <div class="flex justify-center pt-3">
+                  <img
+                    src={product.image}
+                    alt={product.name}
+                    class="w-11/12
+                   h-auto rounded-md	"
+                  />
                 </div>
-                <div>
+                <div
+                  class="flex justify-between text-lg sm:text-xl lg:text-2xl mt-3 pr-2 pl-2
+                 text-teal-800"
+                >
                   <p>{product.name}</p>
                   <p> $ {product.price}</p>
                 </div>
-                <button onClick={() => addProductHandler(product)}>
+                <button
+                  class="mt-6 bg-blue-500 text-zinc-200 p-2 m-1 rounded-md lg:text-lg" 
+                  onClick={() => addProductHandler(product)}
+                >
                   {checkInCart(cart, product) ? "in cart" : " Add to cart"}
                 </button>
               </section>
